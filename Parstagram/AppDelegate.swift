d442291d7da8ba467937cf6fa65f0bbf81839484
Kweku007 <kna38@drexel.edu>
2020-10-23 23:31:06 -0400

    Required User Stories

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // --- Copy this only
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "qeF6SehA0iPZbjx6mzwo69Fco5DR3ogTIOKwmNuo" // <- UPDATED
+                $0.clientKey = "oHKnPKVZJbBlXudlvQoJafAbGjTCyW5kAznSUCEf" // <- UPDATED
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
+
+
         return true
     }
 
